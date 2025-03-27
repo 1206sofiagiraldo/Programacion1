67dfac10769f236b11835d4865b03c75ec6856ee
1206sofiagiraldo <giraldosoja0406@gmail.com>
2025-03-26 20:31:14 -0500

    Cambie el String por el Seller

--- src/main/java/co/edu/umanizales/myapi1/controller/SellerController.java
+++ src/main/java/co/edu/umanizales/myapi1/controller/SellerController.java
@@ -11,19 +11,14 @@ public class
 SellerController {
 
     @GetMapping
-    public String getSeller() {
+    public Seller getSeller() {
+
         Seller pedro = new Seller("1056789067", "Pedro", "Perez", 'M', (byte) 23, "Manizales");
         Seller juan = new Seller("1067834598", "Juan", "Suarez", 'M', (byte) 46, "Pereira");
         Seller juanita = new Seller("1045879323", "Juanita", "Jaramillo", 'F', (byte) 22, "Armenia");
         Seller andrea = new Seller("1053357890", "Andrea", "Lopez", 'F', (byte) 28, "Medellin");
         Seller camilo = new Seller("1038678910", "Camilo", "Rios", 'M', (byte) 24, "Manizales");
 
-        return "Pedro";
-        return "Juan";
-        return "Juanita";
-        return "Andrea";
-        return "Camilo";
-
-
+        return pedro;
     }
 }
