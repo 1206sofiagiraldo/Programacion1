b16f3cb5f852033d773907258bbf3b994124ebe5
1206sofiagiraldo <giraldosoja0406@gmail.com>
2025-04-22 21:56:48 -0500

    CODIGO COMPLETO

--- src/main/java/co/edu/umanizales/myapi1/Myapi1Application.java
+++ src/main/java/co/edu/umanizales/myapi1/Myapi1Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Myapi1Application {
+public class
+Myapi1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Myapi1Application.class, args);
